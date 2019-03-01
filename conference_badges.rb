f2bc5d360be6707eb765c83ad30bc78b3e306491
Audra Tallman <audra.tallman@hotmail.com>
2019-03-01 20:58:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def conference_badges
+  speakers = ["Edsger", "Ada", "Charles", "Alan", ""]
+end
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array_of_names)
+  array_of_names = (badge_maker).to_a
+end
+
+def assign_rooms(speakers)
+  
+end
+
