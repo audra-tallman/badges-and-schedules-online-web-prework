81a2a76f3e24ed89ddf4bb62c1ae92b1040b3899
Audra Tallman <audra.tallman@hotmail.com>
2019-03-01 21:08:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,6 @@
 speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-rooms = [1, 2, 3, 4, 5, 6, 7]
+rooms = [1, 2, 3, 4, 5,]
 def conference_badges(speakers)
-  speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 end
 
 def badge_maker(name)
