5b342f57d2e5f4f5e768f967ae200ddc54c0112a
Audra Tallman <audra.tallman@hotmail.com>
2019-03-01 21:01:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,4 @@
-def conference_badges
+def conference_badges(speakers)
   speakers = ["Edsger", "Ada", "Charles", "Alan", ""]
 end
 
@@ -10,7 +10,9 @@ def batch_badge_creator(array_of_names)
   array_of_names = (badge_maker).to_a
 end
 
-def assign_rooms(speakers)
+def assign_rooms(speakers,room)
+  room (1..7)
+  "Hello, #{name}! You'll be assigned to room #{room}!
   
 end
 
